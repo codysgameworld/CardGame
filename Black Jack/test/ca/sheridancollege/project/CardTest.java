5432c3aea38b10ca74dfa10261048f76c80f0266
codysgameworld <104550724+codysgameworld@users.noreply.github.com>
2023-04-11 12:26:52 -0400

    Updated CardTest

--- Black Jack/test/ca/sheridancollege/project/CardTest.java
+++ Black Jack/test/ca/sheridancollege/project/CardTest.java
@@ -11,39 +11,78 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardTest {
-    
+
     @Test
-    public void testGetSuit() {
+    public void testGetSuitGood() {
+        System.out.println("GetSuit Good Test");
         Card card = new Card(Suit.CLUB, Rank.ACE);
         assertEquals(Suit.CLUB, card.getSuit());
     }
-    
+
     @Test
-    public void testGetRank() {
+    public void testGetSuitBad() {
+        System.out.println("GetSuit Bad Test");
+        Card card = new Card(Suit.CLUB, Rank.ACE);
+        assertNotEquals(Suit.SPADE, card.getSuit());
+    }
+
+    @Test
+    public void testGetRankGood() {
+        System.out.println("GetRank Good Test");
         Card card = new Card(Suit.HEART, Rank.KING);
         assertEquals(Rank.KING, card.getRank());
     }
-    
+
     @Test
-    public void testGetValueOfRank() {
+    public void testGetRankBad() {
+        System.out.println("GetRank Bad Test");
+        Card card = new Card(Suit.HEART, Rank.KING);
+        assertNotEquals(Rank.QUEEN, card.getRank());
+    }
+
+    @Test
+    public void testGetValueOfRankGood() {
+        System.out.println("GetValueOfRank Good Test");
         Card card = new Card(Suit.SPADE, Rank.JACK);
         assertEquals(10, card.getValueOfRank());
     }
-    
+
     @Test
-    public void testToString() {
+    public void testGetValueOfRankBad() {
+        System.out.println("GetValueOfRank Bad Test");
+        Card card = new Card(Suit.SPADE, Rank.JACK);
+        assertNotEquals(11, card.getValueOfRank());
+    }
+
+    @Test
+    public void testToStringGood() {
+        System.out.println("ToString Good Test");
         Card card = new Card(Suit.DIAMOND, Rank.TWO);
         assertEquals("TWO of DIAMOND", card.toString());
     }
-    
+
     @Test
-    public void testEquals() {
+    public void testToStringBad() {
+        System.out.println("ToString Bad Test");
+        Card card = new Card(Suit.DIAMOND, Rank.TWO);
+        assertNotEquals("TWO of CLUB", card.toString());
+    }
+
+    @Test
+    public void testEqualsGood() {
+        System.out.println("Equals Good Test");
         Card card1 = new Card(Suit.CLUB, Rank.ACE);
         Card card2 = new Card(Suit.CLUB, Rank.ACE);
         assertTrue(card1.equals(card2));
-        
-        Card card3 = new Card(Suit.HEART, Rank.KING);
-        assertFalse(card1.equals(card3));
     }
-    
+
+    @Test
+    public void testEqualsBad() {
+        System.out.println("Equals Bad Test");
+        Card card1 = new Card(Suit.CLUB, Rank.ACE);
+        Card card2 = new Card(Suit.SPADE, Rank.ACE);
+        assertFalse(card1.equals(card2));
+    }
+
 }
+
