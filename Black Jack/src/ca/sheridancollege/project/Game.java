a3d4e40c5ef417b12131f7ee3e6f19623029d319
codysgameworld <104550724+codysgameworld@users.noreply.github.com>
2023-04-11 12:25:41 -0400

    Revert "Add files via upload"
    This reverts commit 430268092ca51010631a1a70628cf2aa615ac121.

--- Black Jack/src/ca/sheridancollege/project/Game.java
+++ Black Jack/src/ca/sheridancollege/project/Game.java
@@ -5,7 +5,7 @@
  */
 package ca.sheridancollege.project;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The class that models your game. You should create a more specific child of this class and instantiate the methods
@@ -13,35 +13,84 @@ import java.util.ArrayList;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
+ * @author yeli March 2023
+ * @author Cody Labelle
+ * @author Sarah McCrie
  */
-public abstract class Game {
+public class Game {
 
-    private final String name;//the title of the game
+    private Deck deck = new Deck();
+    private Dealer dealer = new Dealer();
+    private Player player = new Player();
     
-    public Game(String name) {
-        this.name = name;
+    private static Game instance = null;
+
+    private Game() {
+       
     }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
+    public static Game getInstance() {
+        if (instance == null) {
+            instance = new Game();
+        }
+        return instance;
     }
 
-    /**
-     * @return the players of this game
-     */
+    Scanner scan = new Scanner(System.in);
+
+    public void startBlackJack() {
+        String input;
+        do{
+            System.out.println("Welcome to BlackJack! Press s to start the game.");
+            input = scan.nextLine();
+        }
+        while(!input.toLowerCase().equals("s"));
+        
+            startRound();
+            showResult();
+        
+    }
     
+    private void startRound() {
+        deck.createDeck();
+        deck.shuffleDeck();
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+        //player gets 2 cards
+        player.getHand().takeCardFromDeck(deck);
+        player.getHand().takeCardFromDeck(deck);
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+        //dealer gets 2 cards
+        dealer.getHand().takeCardFromDeck(deck);
+        dealer.getHand().takeCardFromDeck(deck);
 
-}//end class
+        dealer.showFirstHand();
+        player.showHand();
+
+        player.hitOrStand(deck);
+    }
+    
+    private void showResult(){
+        if(player.getHand().calculateValue() > 21) {
+            System.out.println("You have bust. You lose.");
+            return;
+        }
+
+        dealer.showHand();
+        while(dealer.getHand().calculateValue() < 17) {
+            dealer.hit(deck);
+        }
+
+        if(dealer.getHand().calculateValue() > 21) {
+            System.out.println("Dealer busts. You win.");
+        } else if(dealer.getHand().calculateValue() > player.getHand().calculateValue()) {
+            System.out.println("You lose.");
+        } else if(player.getHand().calculateValue() > dealer.getHand().calculateValue()) {
+            System.out.println("You win.");
+        } else {
+            System.out.println("Push.");
+        }
+    }
+
+  }
+
+
